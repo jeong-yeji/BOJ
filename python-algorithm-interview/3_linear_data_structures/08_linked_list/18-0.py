478affd1edde33d685b4657f825ef7a409b96345
jeong-yeji <jeongyeji.mail@gmail.com>
2021-10-27 14:17:37 +0900

    18. Odd Even Linked List

---
+++ python-algorithm-interview/3_linear_data_structures/08_linked_list/18-0.py
@@ -0,0 +1,27 @@
+# odd-even-linked-list
+
+# Definition for singly-linked list.
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+
+        odd = head
+        even = even_head = head.next
+
+        while even and even.next:
+            odd.next, even.next = odd.next.next, even.next.next
+            odd, even = odd.next, even.next
+
+        # combine odd with even
+        odd.next = even_head
+        return head
