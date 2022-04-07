ea355fea4befaffa148fc0b39b3770f13c762472
jeong-yeji <jeongyeji.mail@gmail.com>
2022-04-07 23:05:14 +0900

    17. Swap Nodes in Pairs

---
+++ python-algorithm-interview/3_linear_data_structures/08_linked_list/17-2.py
@@ -0,0 +1,16 @@
+# swap-nodes-in-pairs
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# 값만 변경
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        while node and node.next:
+            node.val, node.next.val = node.next.val, node.val
+            node = node.next.next
+        return head
