87e36cda80680f4d5fe041da8ab6e7022283a61d
jeong-yeji <jeongyeji.mail@gmail.com>
2022-05-28 21:37:07 +0900

    32. Number of Islands

---
+++ python-algorithm-interview/4_non_linear_data_structures/12_graph/32-1.py
@@ -0,0 +1,31 @@
+# number of island
+
+class Solution:
+    def numIslands(self, grid) -> int:
+        # 에외 처리
+        # if not grid:
+            # return 0
+
+        def dfs(i: int, j: int):
+            # 더 이상 땅이 아닌 경우 종료
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+
+            grid[i][j] = 0
+
+            # 동서남북 탐색
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+
+        cnt = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    # 모든 육지 탐색 후 카운트 1 증가
+                    cnt += 1
+
+        return cnt
