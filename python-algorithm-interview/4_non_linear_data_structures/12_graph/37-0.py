e23f03fb1e52eed640fc4c086075537c5339e352
jeong-yeji <jeongyeji.mail@gmail.com>
2022-06-27 22:00:57 +0900

    37. subsets

---
+++ python-algorithm-interview/4_non_linear_data_structures/12_graph/37-0.py
@@ -0,0 +1,9 @@
+# subsets
+import itertools
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(nums)+1):
+            res += list(itertools.combinations(nums, i))
+        return res
---
+++ python-algorithm-interview/4_non_linear_data_structures/12_graph/37-1.py
@@ -0,0 +1,13 @@
+# subsets
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(idx, path):
+            res.append(path)
+
+            for i in range(idx, len(nums)):
+                dfs(i + 1, path + [nums[i]])
+
+        dfs(0, [])
+        return res
