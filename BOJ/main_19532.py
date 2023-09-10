37b332ba5a22914fd6c77b4a0756bf4b69e26015
jeong-yeji <jeongyeji.mail@gmail.com>
2023-09-10 17:39:43 +0900

    solution: boj 19532 ver1

---
+++ BOJ/main_19532.py
@@ -0,0 +1,7 @@
+a, b, c, d, e, f = map(int, input().split())
+
+for x in range(-10000, 10001):
+    for y in range(-10000, 10001):
+        if a * x + b * y == c and d * x + e * y == f:
+            print(x, y)
+            break
