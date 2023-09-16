4ea19a8c5e6c74ed90523916a30c9de4a313f426
jeong-yeji <jeongyeji.mail@gmail.com>
2023-09-16 18:28:34 +0900

    solution boj 2559 ver3. w/ two pointer

--- BOJ/main_2559.py
+++ BOJ/main_2559.py
@@ -1,11 +1,6 @@
 k, n = map(int, input().split())
 temperature = list(map(int, input().split()))
-
-prefix = [0 for _ in range(k + 2)]
-for i in range(k):
-    prefix[i + 1] = prefix[i] + temperature[i]
-
-answer = -100 * 100000
-for i in range(n, k + 1):
-    answer = max(answer, prefix[i] - prefix[i - n])
-print(answer)
+answer = [sum(temperature[:n])]
+for i in range(n, k):
+    answer.append(answer[-1] + temperature[i] - temperature[i - n])
+print(max(answer))
