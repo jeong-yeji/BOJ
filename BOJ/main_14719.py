de0063d0b798b824749cfda5eb385cb968a6c0e6
jeong-yeji <jeongyeji.mail@gmail.com>
2023-09-17 01:35:51 +0900

    solution boj 14719 ver2. w/ two pointer

--- BOJ/main_14719.py
+++ BOJ/main_14719.py
@@ -1,8 +1,19 @@
 h, w = map(int, input().split())
 heights = list(map(int, input().split()))
+
 answer = 0
-for i in range(1, w - 1):
-    left = max(heights[:i + 1])
-    right = max(heights[i:])
-    answer += abs(heights[i] - min(left, right))
+left, right = 0, w - 1
+max_left, max_right = heights[left], heights[right]
+
+while left < right:
+    max_left = max(max_left, heights[left])
+    max_right = max(max_right, heights[right])
+
+    if max_left >= max_right:
+        answer += max_right - heights[right]
+        right -= 1
+    if max_left < max_right:
+        answer += max_left - heights[left]
+        left += 1
+        
 print(answer)
