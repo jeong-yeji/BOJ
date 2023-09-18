92e8972b4a1645cb9f4eacd1d81c7ababf26d2ca
jeong-yeji <jeongyeji.mail@gmail.com>
2023-09-18 22:05:45 +0900

    solution boj 15656 ver1

---
+++ BOJ/main_15656.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort()
+arr = []
+
+
+def recur(k, l):
+    if k == m:
+        print(*arr)
+        return
+    for i in range(n):
+        arr.append(numbers[i])
+        recur(k + 1, i + 1)
+        arr.pop()
+
+
+recur(0, 0)
