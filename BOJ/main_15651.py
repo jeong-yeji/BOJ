a259ff4b3db587f22a3679eef20b6a7c50def432
jeong-yeji <jeongyeji.mail@gmail.com>
2023-09-18 21:41:32 +0900

    solution boj 15651 ver1.

---
+++ BOJ/main_15651.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+arr = []
+
+
+def recur(k):
+    if k == m:
+        print(*arr)
+        return
+    for i in range(1, n + 1):
+        arr.append(i)
+        recur(k + 1)
+        arr.pop()
+
+
+recur(0)
